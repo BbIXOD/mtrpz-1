122909146cbf221f28fb7bed93cca412699dab9b
BbIXOD <maksdyachok2005@gmail.com>
2024-02-24 20:13:22 +0200

    Revert "Start adding oop"
    This reverts commit 3f892f84097f1704fbec34ae27658d5c3a7f2692.
    reject oop, embrace procedural programming

--- Tag.js
+++
@@ -1,8 +0,0 @@
-export default class Tag {
-  constructor(markdownOpener, markdownCloser, htmlOpener, htmlCloser) {
-    this.markdownOpener = markdownOpener
-    this.markdownCloser = markdownCloser
-    this.htmlOpener = htmlOpener
-    this.htmlCloser = htmlCloser
-  }
-}
