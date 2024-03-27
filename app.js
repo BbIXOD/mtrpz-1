3b68cf175740b0f8a7812a9f230383a118d413a0
Maksym Diachok <108470719+BbIXOD@users.noreply.github.com>
2024-03-27 15:21:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ program
   .version("1.0.0")
   .argument('<inputFile>', 'Input markdown file')
   .option("-o, --out <outputFile>", "Write result tp given file")
-  .option("-f, --format", "Html/Escape codes", false)
+  .option("-f, --format [format]", "Html/Escape codes", "html")
   .description("Convert markdown to html")
   .parse(process.argv);
 
 const { out, format } = program.opts();
 
+if (format != "html" && format != "esc") throw new Error("Invalid type")
+
 const content = fs.readFileSync(program.args[0], "utf8");
-const result = replacer(content, format ? 1 : 0);
+const result = replacer(content, "html" ? 0 : 1);
 
 if (out) {
   fs.writeFileSync(out, result, "utf8");
